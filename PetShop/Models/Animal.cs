e5e0e112441fe4c1ccef45d318b34e01e6feca36
Victor Hugo <vhfn2004@gmail.com>
2023-11-22 14:50:32 -0300

    projeto iniciado

---
+++ PetShop/Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace PetShop.Models
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public string Porte { get; set; }
+
+        public Cliente Cliente { get; set; }
+        public int ClienteId { get; set; }
+
+    }
+}
---
+++ PetShop/Models/Servico.cs
@@ -0,0 +1,10 @@
+﻿namespace PetShop.Models
+{
+    public class Servico
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public Animal Animal { get; set; }
+        public int AnimalId { get; set; }
+    }
+}
